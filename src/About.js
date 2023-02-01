c41aa97c9259ff82b3935d085199cbeedd1fd8d5
ajrego13 <83869597+ajrego13@users.noreply.github.com>
2023-02-01 12:59:26 -0500

    added about page and refactored some code

--- src/About.js
+++ src/About.js
@@ -2,8 +2,8 @@
 const About = () => {
 
     return (
-        <div className="about_container"> 
-            <h1 className="about_p">About Me</h1>
+        <div id="about"  className="about_container"> 
+            <h1  className="about_p">About Me</h1>
             <p>Hello! My name is Alex and I enjoy creating mobile first web applications.
                 
             </p>
--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
          <header className="header">
             <div className="container__header">
                 <p className="header_p_top">Hi, my name is</p>
-                <h1 id="about" className="header_h1">Alex Rego.</h1>
+                <h1 className="header_h1">Alex Rego.</h1>
                 <h2 className="header_h1">I Buiild things for the web.</h2>
                 <p className="header_p_bottom">I'm a software engineer specializing in building exceptional digital experiences.</p>
             </div>
